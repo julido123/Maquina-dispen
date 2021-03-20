c69f0f68f81b03749e26419424884101118c02a7
julido123 <79119575+julido123@users.noreply.github.com>
2021-03-19 20:14:11 -0500

    Add files via upload

---
+++ TallerMaquinaDispensadora/src/maquinaDispensadora/app/Aplicación.java
@@ -0,0 +1,22 @@
+package maquinaDispensadora.app;
+
+import maquinaDispensadora.dominio.MaquinaDispensadora;
+
+public class Aplicación {
+    public static void main(String[] args) {
+
+        MaquinaDispensadora maqui=new MaquinaDispensadora("autosnack","Negro",10, 8);
+
+        System.out.println(maqui.verProductos());
+
+
+        maqui.sacarProducto("cheestres");
+        maqui.sacarProducto("cheestres");
+        maqui.sacarProducto("cheestres");
+
+
+
+    }
+}
+
+
